2513651cc27ccb4cd1184dbc3d9786932a0f8f03
RalfRendelf <vladislavungvicky@gmail.com>
2022-08-15 16:27:16 +0700

    123

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -2,40 +2,14 @@
 
 #include <iostream>
 #include<array>
-#include "Solution.h"
-#define null NULL
+#include<vector>
+#include "XY.h"
+//#include "Solution.h"
+using namespace std;
 
 int main()
 {
-	/*vector<int> g{ 3,9,20,15,7 }, f{ 9,3,15,20,7 };
-	Solution a;
-	auto az= a.buildTree(g, f);*/
-
-	Solution a;
-	TreeNode arr;
-	vector<TreeNode> g;
-	int k[] = { 1,2,2,3,3,null,null,4,4 };
-		for (auto d= 0 ; d < sizeof(k); ++d)
-		{ 
-			g.push_back(arr);
-			g[d].val = k[d];
-			
-			if (d > 0)
-			{
-				if (d % 2 == 1)
-				{
-
-				}
-				else
-				{
-
-				}
-			}
-		
-			
-		}
-	
-	
+	upr947a();
 	
 }
 
--- ConsoleApplication7/XY.h
+++ ConsoleApplication7/XY.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Screen.h"
+#include <string>
+
 
 
 
@@ -17,3 +19,65 @@ class Y
 	
 };
 X f(12);
+void upr947()
+{
+	string g("ab2c3d7R4E6"),g1 = g;
+	string s("0123456789");
+	bool check = true;
+	int cnt = 0;
+	vector<int> stat;
+	while (check)
+	{
+		int i;
+		
+		if (g1.find_first_of(s) == string::npos)
+		{
+			check = 0;
+		}
+		else
+		{
+		
+		i = g1.find_first_of(s);
+		g1.erase(g1.begin() + i);
+		stat.push_back(i + cnt++);
+		
+		}
+
+	}
+	//cout << g1;
+	for (auto i : stat)
+	{
+		cout << i << " ";
+	}
+}
+void upr947a()
+{
+	string g("ab2c3d7R4E6"), g1 = g;
+	string s("0123456789");
+	bool check = true;
+	int cnt = 0;
+	vector<int> nums, symbols;
+	while (check)
+	{
+		int i;
+
+		if (g1.find_first_not_of(s) == string::npos)
+		{
+			check = 0;
+		}
+		else
+		{
+
+			i = g1.find_first_not_of(s);
+			g1.erase(g1.begin() + i);
+			symbols.push_back(i + cnt++);
+
+		}
+
+	}
+	cout << g1 << '\n';
+	for (auto i : symbols)
+	{
+		cout << i << " ";
+	}
+}
