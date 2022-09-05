6488778659a5ebb52759b6977b2d8f9d0a912a19
RalfRendelf <vladislavungvicky@gmail.com>
2022-09-05 11:57:45 +0700

    123

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -4,12 +4,35 @@
 #include<array>
 #include<vector>
 #include "XY.h"
-//#include "Solution.h"
+#include <iostream>
+#include <bitset>
+#include <bit>
+#include <cassert>
+#include <stdexcept>
 using namespace std;
-
 int main()
 {
-	upr947a();
-	
-}
+    std::bitset<10> b1("1111110000");
 
+    std::size_t idx = 0;
+    while (idx < b1.size() && !b1.test(idx)) {
+        ++idx;
+    }
+
+    assert(static_cast<int>(idx) == std::countr_zero(b1.to_ulong()));
+
+    if (idx < b1.size()) {
+        std::cout << "first set bit at index " << idx << '\n';
+    }
+    else {
+        std::cout << "no set bits\n";
+    }
+
+    try {
+        if (b1.test(b1.size()))
+            std::cout << "Expect unexpected!\n";
+    }
+    catch (std::out_of_range const& ex) {
+        std::cout << "Exception: " << ex.what() << '\n';
+    }
+}
--- ConsoleApplication7/XY.h
+++ ConsoleApplication7/XY.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "Screen.h"
 #include <string>
-
-
-
+#include <stack>
+#include <list>
+#include <algorithm>
+#include <set>
+#include <math.h>
+#include <numeric>
 
 class X
 {
@@ -81,3 +84,151 @@ void upr947a()
 		cout << i << " ";
 	}
 }
+string reverse(string const& s)
+{
+	string rev(s.rbegin(), s.rend());
+	return rev;
+}
+
+void stackchek()
+{
+	stack<char> f;
+	string g = "1234567890()+-*/", g1 = "1234567890";
+	char c;
+	while(cin>>c)
+	{
+		if (g.find_first_of(c) != string::npos)
+		{
+			f.push(c);
+		}
+	}
+	
+	string f1;
+	bool pr = 0;
+	while (!f.empty())
+	{
+		if (f.top() == ')')
+		{
+			pr = 1;}
+		if (pr) {
+			if (f.top() == '(') pr = 0;
+			f1.push_back(f.top());
+			}
+		f.pop();
+	}
+	
+	f1 = reverse(f1);
+	cout << f1;
+	
+	
+	
+}
+void upr106()
+{
+	vector<int> g{ 1,2,3,4,5,6,7 };
+	fill_n(g.begin(), g.size(), 1);
+	for (auto a : g)
+		cout << a;
+	list<int> f;
+ 
+}
+void upr107a()
+{
+	vector<int> vec;
+	list<int> lst; 
+	int i;
+	while (cin >> i)
+		lst.push_back(i);
+	system("pause");
+	auto it = back_inserter(vec);
+	copy(lst.begin(), lst.end(), it);
+	for (auto a: vec)
+	{
+		cout << a;
+	}
+}
+void upr107b()
+{
+	vector<int> vec;
+	vec.reserve(10);
+	auto it = back_inserter(vec);
+	fill_n(it,10, 0);
+	
+	for (auto a : vec)
+
+		cout << a;
+}
+//class Solution {
+//public:
+//	int countDifferentSubsequenceGCDs(vector<int>& nums) {
+//		int max = 0;
+//
+//		bitset<200001> bs;
+//		for (int i : nums) {
+//			max = std::max(i, max);
+//			bs.set(i);
+//		}
+//
+//
+//		for (int i = 1; i <= max; i++) {
+//			int gcd = 0;
+//			for (int j = i; j <= max; j += i) {
+//				if (bs.test(j)) {
+//					gcd = std::gcd(j, gcd);
+//				}
+//			}
+//			if (gcd == i) {
+//				m_gcds.insert(gcd);
+//			}
+//		}
+//
+//		return m_gcds.size();
+//	}
+//
+//private:
+//	std::set<int> m_gcds;
+//};
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution1 {
+public:
+	void helper(TreeNode* root, vector<vector<double>>& alvec,int lvl)
+	{
+		vector<double> val;
+		if (alvec.size() < lvl)
+			alvec.push_back(val);
+		alvec[lvl-1].push_back(root->val);
+		if (root->left != NULL)
+			helper(root->left, alvec, lvl + 1);
+		if (root->right != NULL)
+			helper(root->right, alvec, lvl + 1);
+
+	
+	}
+	vector<double> helper(vector<vector<double>>& alvec)
+	{
+		vector<double> vec;
+		int i;
+		for (auto& a : alvec)
+		{
+			i = accumulate(a.begin(), a.end(),-5000000000);
+			vec.push_back(i / a.size());
+
+		}
+		return vec;
+
+}
+	
+	vector<double> averageOfLevels(TreeNode* root) {
+		vector<vector<double>> valof;
+	helper(root, valof, 1);
+		return helper(valof);
+	}
+};
